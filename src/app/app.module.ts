4efd58e3463a9da67c02ce5e6a0d7ae2b8b0b34b
liran <liranida@gmail.com>
2019-05-01 12:48:46 +0300

    commit-save trivia game

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { Item1Component } from './item1/item1.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Item1Component
   ],
   imports: [
     BrowserModule
